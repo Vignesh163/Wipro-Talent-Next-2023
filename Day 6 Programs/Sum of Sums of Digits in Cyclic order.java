ed930f8b2a1ca3b82f1c08f0fc8c258631e78937
VIGNESH U <89632028+Vignesh163@users.noreply.github.com>
2023-02-11 09:38:47 +0530

    Update Sum of Sums of Digits in Cyclic order.java

--- Day 6 Programs/Sum of Sums of Digits in Cyclic order.java
+++ Day 6 Programs/Sum of Sums of Digits in Cyclic order.java
@@ -1,11 +1,13 @@
 import java.io.*;
-
 import  java.util.*;
-
 class UserMainCode{
-
 public int sumOfSumsOfDigits(int input1){             
-
 String str=Integer.toString(input1); 
-
-int sum=0; 
+int sum=0;
+  for(int i=0;i<str.length();i++){
+    for(int j=0;j<str.length();j++){
+      int num=Charactere].getNumericValue(str.charAt(j));
+      sum+=num;
+}}
+  return sum;
+}}
